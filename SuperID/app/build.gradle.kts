c164e72efc65c7864b69f0eccc61a65eac3b0303
Lucas De Campos Ranzani <lucasdecamposranzani@gmail.com>
2025-04-04 10:41:39 -0300

    Rebuild dos plugins do grandle

--- SuperID/app/build.gradle.kts
+++ SuperID/app/build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
-
     id("com.google.gms.google-services")
-
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -13,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "br.edu.puccampinas.pi3.turma4.superid"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
