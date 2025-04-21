71afa492f328d30d41b8b00a224c962319f5f40a
Lucas De Campos Ranzani <lucasdecamposranzani@gmail.com>
2025-04-21 17:21:02 -0300

    feat: Nova versão do UI das telas de SingUp e Welcome

--- SuperID/app/src/main/java/br/edu/puccampinas/pi3/turma4/superid/ui/theme/Color.kt
+++ SuperID/app/src/main/java/br/edu/puccampinas/pi3/turma4/superid/ui/theme/Color.kt
@@ -10,11 +10,6 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-object WelcomeColors {
-    val backgroundColor = Color(0xFF0E0F1C)
-    val primaryBlue = Color(0xFF3E61FE)
-    val textColor = Color(0xFFCED1D9)
-}
 
 object SingInColors {
     val backgroundColor = Color(0xFF121212)
