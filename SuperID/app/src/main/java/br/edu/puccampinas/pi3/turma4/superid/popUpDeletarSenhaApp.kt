12169b39ff053e0d1d00ff835524e2ca454d2c46
batista29 <natabatista2908@gmail.com>
2025-05-25 12:50:28 -0300

    Importações e Código organizados

--- SuperID/app/src/main/java/br/edu/puccampinas/pi3/turma4/superid/popUpDeletarSenhaApp.kt
+++ SuperID/app/src/main/java/br/edu/puccampinas/pi3/turma4/superid/popUpDeletarSenhaApp.kt
@@ -2,91 +2,33 @@ package br.edu.puccampinas.pi3.turma4.superid
 
 import android.os.Bundle
 import android.util.Log
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import br.edu.puccampinas.pi3.turma4.superid.ui.theme.SuperIDTheme
-
-//fire
-import com.google.firebase.firestore.FirebaseFirestore
-
-
-//Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Warning
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 
+import br.edu.puccampinas.pi3.turma4.superid.ui.theme.SuperIDTheme
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.launch
-
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 class popUpDeletarSenhaApp : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
